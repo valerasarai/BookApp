ba40706ab871f33f8118bb0b4533144465c26e92
Valera Sarai <valera.satrai@gmailcom>
2017-09-19 19:30:31 +0300

    test calass updated

--- src/test/java/com/valerastudy/BookApp/DbImplTest.java
+++ src/test/java/com/valerastudy/BookApp/DbImplTest.java
@@ -3,24 +3,27 @@ package com.valerastudy.BookApp;
 import com.valerastudy.BookApp.database.DbImpl;
 import com.valerastudy.BookApp.model.Book;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DbImplTest {
     Book book = new Book(1,"Book 1","Author 1", 200);
 
     @Test
-    public void saveBookTest() {
+    public void test01_saveBookTest() {
         Assert.assertEquals(true, (DbImpl.saveBook(book) >= 0));
     }
 
     @Test
-    public void updateBookTest(){
+    public void test02_updateBookTest(){
         book.setNrOfPages(10);
         Assert.assertEquals(true, DbImpl.updateBook(book));
     }
 
     @Test
-    public void deleteById(){
+    public void test03_deleteById(){
         book.setBookId(2);
         DbImpl.saveBook(book);
         Assert.assertEquals(true, DbImpl.deleteById(book.getBookId()));
